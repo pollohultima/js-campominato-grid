fc6373fba283ef70689dc6f4a75dd8c358160e16
pollohultima <matteo.polonio@gmail.com>
2021-10-20 17:07:09 +0200

    Final Commit all works!

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,51 +4,54 @@ const easy = 100
 const medium = 81
 const hard = 49
 
-
+const contEl = document.querySelector('.container')
 
 
 document.getElementById("easy").addEventListener('click', function () {
     contEl.classList.add("easy_cont")
     document.querySelector('.buttons').style.display = 'none';
-
+    document.querySelector('.container').style.visibility = 'visible'
+    generaGriglia(easy)
 })
 
 document.getElementById("medium").addEventListener('click', function () {
     contEl.classList.add("medium_cont")
     document.querySelector('.buttons').style.display = 'none';
+    document.querySelector('.container').style.visibility = 'visible'
+    generaGriglia(medium)
+
 })
 
 document.getElementById("hard").addEventListener('click', function () {
     contEl.classList.add("hard_cont")
     document.querySelector('.buttons').style.display = 'none';
+    document.querySelector('.container').style.visibility = 'visible'
+    generaGriglia(hard)
 })
 
 
+function generaGriglia(dimensione) {
 
+    for (let i = 1; i <= dimensione; i++) {
 
+        //creo il div
+        const divElement = document.createElement('div')
 
-/* Creare in js una griglia 8x8
-   ogni volta che clicco un quadrato questo si colora di verde */
+        divElement.innerHTML = `${i}`
 
-//DOM 
-const contEl = document.querySelector('.container')
+        //assegno classe
+        divElement.className = 'cella'
 
-//Loop - genero una griglia da 64 elementi con ciclo
-for (let i = 0; i < easy; i++) {
+        //inserisco cella dentro container
+        contEl.append(divElement)
 
-    //creo il div
-    const divElement = document.createElement('div')
+        //Event Listener a ciascun elemento creato nel ciclo
+        divElement.addEventListener('click', function () {
+            //assegno colore verde all'interno di questa funzione con 'this' 
+            this.style.backgroundColor = 'cornflowerblue';
 
-    //assegno classe
-    divElement.className = 'cella'
+        })
+    }
 
-    //inserisco cella dentro container
-    contEl.append(divElement)
-
-    //Event Listener a ciascun elemento creato nel ciclo
-    divElement.addEventListener('click', function () {
-        //assegno colore verde all'interno di questa funzione con 'this' 
-        this.style.backgroundColor = 'green';
-
-    })
 }
+
